d12bce314b6328cbd0c46363278a30f1f81d6326
LucieO <luciecamilleory@gmail.com>
2020-04-06 17:50:38 +0200

    Updates

---
+++ precache-manifest.e3f78367d5c92c30bcf0c60a6b1d90be.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3940134237285b415bd39150bcb0a670",
+    "url": "/Lucieo/esquisse-front/index.html"
+  },
+  {
+    "revision": "5c583662f2ae3ff624c7",
+    "url": "/Lucieo/esquisse-front/static/css/main.ebeb01ef.chunk.css"
+  },
+  {
+    "revision": "5d2829d07a86fe7cafed",
+    "url": "/Lucieo/esquisse-front/static/js/2.08f32449.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/Lucieo/esquisse-front/static/js/2.08f32449.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c583662f2ae3ff624c7",
+    "url": "/Lucieo/esquisse-front/static/js/main.7e198bcf.chunk.js"
+  },
+  {
+    "revision": "5e3322db0217f086f865",
+    "url": "/Lucieo/esquisse-front/static/js/runtime-main.94c26982.js"
+  },
+  {
+    "revision": "88839167662afe26163402dff9bbdfc7",
+    "url": "/Lucieo/esquisse-front/static/media/artist.88839167.gif"
+  },
+  {
+    "revision": "b8039cd29903a853f733e03f996d5bdf",
+    "url": "/Lucieo/esquisse-front/static/media/crayons.b8039cd2.jpeg"
+  },
+  {
+    "revision": "b5d684c5623a70656e9cb7ca4291ea78",
+    "url": "/Lucieo/esquisse-front/static/media/gearLoader.b5d684c5.gif"
+  },
+  {
+    "revision": "225f578361ae9121e4a7d5498d50cba2",
+    "url": "/Lucieo/esquisse-front/static/media/lapin.225f5783.png"
+  },
+  {
+    "revision": "1d7626e6cbbdc02be15b16fa36add9ae",
+    "url": "/Lucieo/esquisse-front/static/media/loader.1d7626e6.gif"
+  },
+  {
+    "revision": "042e7439a6f88e6017ae50349bb80011",
+    "url": "/Lucieo/esquisse-front/static/media/pencil.042e7439.svg"
+  },
+  {
+    "revision": "bb668a20c5cfaf6b76eac3eb954c37cb",
+    "url": "/Lucieo/esquisse-front/static/media/sherlock.bb668a20.png"
+  },
+  {
+    "revision": "364ca7263474641093b931c33b4990a1",
+    "url": "/Lucieo/esquisse-front/static/media/wrong.364ca726.png"
+  }
+]);
